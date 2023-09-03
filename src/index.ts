ae4ad53eb279817f7a9ceb996dae382aeeb1043d
Guo Yunhe <i@guoyunhe.me>
2023-09-03 13:33:29 +0800

    install

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
+import { spawnSync } from 'node:child_process';
 import { join } from 'node:path';
+import { cwd } from 'node:process';
 
 export interface Options {
-  template?: number;
+  template?: string;
   install?: true | string;
 }
 
-export function create(project: string | null, { template, install }: Options) {
-  const projectFullPath = project ? join(process.cwd(), project) : process.cwd();
+export function create(
+  project: string | null,
+  { template = '@vited/template-react', install }: Options
+) {
+  const projectFullPath = project ? join(cwd(), project) : cwd();
+  const templatePath = join(cwd(), 'node_modules', template);
+
+  if (install) {
+    let installCommand;
+    switch (install) {
+      case true:
+      case 'npm':
+        installCommand = 'npm install';
+        break;
+      case 'yarn':
+        installCommand = 'yarn';
+        break;
+      default:
+        installCommand = install + ' install';
+    }
+    spawnSync(installCommand);
+  }
 }
