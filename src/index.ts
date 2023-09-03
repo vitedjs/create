05ab206fb3f869b0bc0dd7ef443ebc27f112830e
Guo Yunhe <i@guoyunhe.me>
2023-09-03 14:17:39 +0800

    install package

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,57 @@
-import { spawnSync } from 'node:child_process';
+import chalk from 'chalk';
+import { spawn } from 'node:child_process';
 import { join } from 'node:path';
-import { cwd } from 'node:process';
+import { cwd, exit } from 'node:process';
 
 export interface Options {
   template?: string;
   install?: true | string;
 }
 
-export function create(
+export async function create(
   project: string | null,
   { template = '@vited/template-react', install }: Options
 ) {
-  const projectFullPath = project ? join(cwd(), project) : cwd();
+  const projectPath = project ? join(cwd(), project) : cwd();
+  console.log(projectPath);
   const templatePath = join(cwd(), 'node_modules', template);
 
   if (install) {
-    let installCommand;
-    switch (install) {
-      case true:
+    installPackages(projectPath, install as string).catch(() => {
+      exit(1);
+    });
+  }
+}
+
+export function installPackages(projectPath: string, packageManager: string) {
+  return new Promise<void>((resolve, reject) => {
+    let command: string;
+
+    switch (packageManager) {
       case 'npm':
-        installCommand = 'npm install';
+        command = 'npm install';
         break;
       case 'yarn':
-        installCommand = 'yarn';
+        command = 'yarn';
         break;
       default:
-        installCommand = install + ' install';
+        command = packageManager + ' install';
     }
-    spawnSync(installCommand);
-  }
+
+    console.log(chalk.green(`\n📦 ${command}\n`));
+
+    const child = spawn(command, {
+      cwd: projectPath,
+      shell: true,
+      stdio: 'inherit',
+    });
+
+    child.on('close', function (code) {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  });
 }
